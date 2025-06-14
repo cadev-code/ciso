072b6a4fc722a54ed51903070af5f6735efff21c
Carlos Alberto <carlosdev1810@gmail.com>
2025-06-13 19:11:29 -0600

    chore(server): improve port configuration using enviroment variables and dotenv

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {});
